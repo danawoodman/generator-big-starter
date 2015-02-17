6afc5e9677f247a073ffc1fbca5a16ef73ad3160
z <z@zaybiz.com>
2015-02-17 07:51:37 -0800

    Update tests

--- test/test-app.js
+++ test/test-app.js
@@ -35,26 +35,21 @@ describe('big-starter:app', function () {
 
   it('sets up scripts', function () {
     assert.file([
-      'src/index.jsx'
+      'src/frontend/index.jsx'
     ]);
     assert.fileContent('package.json', /jsx-loader/);
   });
 
   it('sets up HTML', function () {
     assert.file([
-      'src/index.html',
+      'src/frontend/index.html',
     ]);
-    //assert.fileContent('README.md', /Starter/);
   });
 
   it('setups up styles', function () {
     assert.file([
-      'tasks/styles.js',
-      'src/index.less'
+      'src/frontend/index.less'
     ]);
-    assert.fileContent('package.json', /gulp-less/);
-    assert.fileContent('package.json', /gulp-rename/);
-    assert.fileContent('package.json', /gulp-sourcemaps/);
   });
 
   it('creates README', function () {
@@ -64,64 +59,23 @@ describe('big-starter:app', function () {
     assert.fileContent('README.md', /Starter/);
   });
 
-  it('configure watch task', function () {
-    assert.file([
-      'tasks/watch.js'
-    ]);
-  });
-
-  it('configure dist task', function () {
-    assert.file([
-      'tasks/dist.js'
-    ]);
-  });
-
   it('configure fb-flo', function () {
     assert.file([
       'flo.js',
-      'tasks/flo.js'
     ]);
     assert.fileContent('package.json', /fb-flo/);
-    assert.fileContent('tasks/watch.js', /flo/);
-  });
-
-  it('configure serve task', function () {
-    assert.file([
-      'tasks/serve.js'
-    ]);
-    assert.fileContent('package.json', /gulp-serve/);
   });
 
   it('configures test suite', function () {
     assert.file([
-      'tasks/test.js',
       'test/app-test.js'
     ]);
-    assert.fileContent('package.json', /gulp-mocha/);
     assert.fileContent('package.json', /should/);
-    assert.fileContent('tasks/test.js', /tests\.testsPath/);
-    assert.fileContent('tasks/test.js', /tests\.watchPath/);
-    assert.fileContent('tasks/config.js', /testsPath/);
-  });
-
-  it('configures gulp', function () {
-    assert.file([
-      'gulpfile.js',
-      'tasks/config.js',
-      'tasks/html.js',
-      'tasks/clean.js'
-    ]);
-    assert.fileContent('gulpfile.js', /'watch'/);
-    assert.fileContent('package.json', /gulp/);
-    assert.fileContent('package.json', /del/);
-    assert.fileContent('package.json', /gulp-util/);
-    assert.fileContent('package.json', /require-dir/);
   });
 
   it('configures WebPack', function () {
     assert.file([
       'webpack.config.js',
-      'tasks/webpack.js',
     ]);
     assert.fileContent('package.json', /webpack/);
     assert.fileContent('package.json', /webpack-dev-server/);
